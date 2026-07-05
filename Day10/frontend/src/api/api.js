fc8cb10253b99bc1bceabc77d491e5dbd0d9e98e
harsh8520 <harshasoriya258@gmail.com>
2026-07-05 23:17:37 +0530

    Update

--- Day10/frontend/src/api/api.js
+++ Day10/frontend/src/api/api.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-console.log("Vite url " + import.meta.env.VITE_API_URL);
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true
